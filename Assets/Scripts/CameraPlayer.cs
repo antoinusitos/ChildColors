edb1eb89c87ac5ed9b0a46ab68adf5162418e6f5
UBISOFT-ORG\agargasson <antoine.gargasson@ubisoft.com>
2018-10-10 18:32:04 +0200

    testing shaders, added shader forge

---
+++ Assets/Scripts/CameraPlayer.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPlayer : MonoBehaviour 
+{
+    #region Public Fields
+    public Transform transformToRotate = null;
+    public float _speedHorizontal = 100.0f;
+    #endregion
+
+    #region Private Fields
+    private Transform _transform = null;
+
+    private float _mouseSensibility = 0.01f;
+    private float _stickSensibility = 0;
+
+    private bool _invertMouse = true;
+
+    public bool canMoveAtStart = true;
+    private bool _canMove = true;
+    #endregion
+
+    #region Unity Methods
+    private void Start () 
+	{
+        _transform = transform;
+        _canMove = canMoveAtStart;
+    }
+	
+	private void Update () 
+	{
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            _invertMouse = !_invertMouse;
+        }
+
+        if (!_canMove) return;
+
+        float axis = Input.GetAxis("Mouse X");
+
+        if (axis > _mouseSensibility || axis < -_mouseSensibility)
+        {
+            float amount = axis * _speedHorizontal * Time.deltaTime;
+            _transform.Rotate(Vector3.up, amount);
+        }
+
+        float axisTop = Input.GetAxis("Mouse Y");
+        if (_invertMouse) axisTop *= -1;
+
+        transformToRotate.Rotate(Vector3.right, axisTop);
+    }
+	#endregion
+	
+	#region Public Methods
+    #endregion
+	
+	#region Private Methods
+    #endregion
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour 
+{
+    #region Public Fields
+    public float _speed = 4.0f;
+    public bool canMoveAtStart = true;
+    #endregion
+
+    #region Private Fields
+    private Rigidbody _rigidBody = null;
+
+    private Transform _transform = null;
+
+    private bool _canMove = true;
+
+    private float _stickSensibility = 0;
+    #endregion
+
+    #region Unity Methods
+    private void Start () 
+	{
+        _rigidBody = GetComponent<Rigidbody>();
+        _transform = transform;
+        _canMove = canMoveAtStart;
+    }
+	
+	private void FixedUpdate () 
+	{
+        Vector3 newPos = Vector3.zero;
+        bool movement = false;
+        if (Input.GetKey(KeyCode.Z))
+        {
+            movement = true;
+            newPos += _transform.forward;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            movement = true;
+            newPos -= _transform.forward;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            newPos += _transform.right;
+            movement = true;
+        }
+        else if (Input.GetKey(KeyCode.Q))
+        {
+            newPos -= _transform.right;
+            movement = true;
+        }
+
+        if (movement)
+        {
+            newPos.Normalize();
+            _rigidBody.MovePosition(_rigidBody.position + newPos * Time.deltaTime * _speed);
+        }
+    }
+	#endregion
+	
+	#region Public Methods
+    #endregion
+	
+	#region Private Methods
+    #endregion
+}
