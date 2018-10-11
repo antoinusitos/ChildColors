ef1b396ec7773b0b27d3fb1fd1fee25acbb19bc5
UBISOFT-ORG\agargasson <antoine.gargasson@ubisoft.com>
2018-10-11 18:18:12 +0200

    added load level, need to change receive laser color

---
+++ Assets/Scripts/ChangeLevel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeLevel : MonoBehaviour 
+{
+    #region Public Fields
+    public int nextLevel = 0;
+    #endregion
+
+    #region Private Fields
+    #endregion
+
+    #region Unity Methods
+    private void OnTriggerEnter(Collider other)
+    {
+        Player p = other.GetComponent<Player>();
+        if(p != null)
+        {
+            SceneManager.LoadScene(nextLevel);
+        }
+    }
+    #endregion
+
+    #region Public Methods
+    #endregion
+
+    #region Private Methods
+    #endregion
+}
