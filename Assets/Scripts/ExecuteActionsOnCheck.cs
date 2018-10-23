97fefb9a29c75dde8838b9c0732f37a128644447
UBISOFT-ORG\agargasson <antoine.gargasson@ubisoft.com>
2018-10-23 16:03:26 +0200

    added trigger check to load room

---
+++ Assets/Scripts/ExecuteActionsOnCheck.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ExecuteActionsOnCheck : MonoBehaviour 
+{
+    #region Public Fields
+    public UnityEvent events = null;
+    public bool executeMultipleTime = false;
+    #endregion
+
+    #region Private Fields
+    protected bool _conditionCompleted = false;
+    protected bool _triggered = false;
+	#endregion
+	
+	#region Unity Methods	
+	private void Update () 
+	{
+        LocalUpdate();
+
+        if (_conditionCompleted && !_triggered)
+        {
+            if(!executeMultipleTime)
+                _triggered = true;
+            events.Invoke();
+        }
+	}
+	#endregion
+	
+	#region Public Methods
+    #endregion
+	
+	#region Private Methods
+    protected virtual void LocalUpdate()
+    {
+
+    }
+    #endregion
+}
---
+++ Assets/Scripts/ExecuteActionsOnCheckTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExecuteActionsOnCheckTrigger : ExecuteActionsOnCheck
+{
+    #region Public Fields
+    public TriggerCheckObject trigger = null;
+    #endregion
+
+    #region Private Fields
+    #endregion
+
+    #region Unity Methods
+    #endregion
+
+    #region Public Methods
+    #endregion
+
+    #region Private Methods
+    protected override void LocalUpdate()
+    {
+        _conditionCompleted = trigger.GetObjectInside();
+    }
+    #endregion
+}
