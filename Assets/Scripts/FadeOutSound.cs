a364f6b03849f481c5ab0f87765203c6375cd4f4
UBISOFT-ORG\agargasson <antoine.gargasson@ubisoft.com>
2018-10-24 15:42:00 +0200

    level 2 done

---
+++ Assets/Scripts/FadeOutSound.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeOutSound : MonoBehaviour 
+{
+    #region Public Fields
+    public AudioSource audioSource = null;
+	#endregion
+	
+	#region Private Fields
+	#endregion
+	
+	#region Unity Methods
+	#endregion
+	
+	#region Public Methods
+    public void FadeOut()
+    {
+        StartCoroutine("Fade");
+    }
+    #endregion
+	
+	#region Private Methods
+    private IEnumerator Fade()
+    {
+        float timer = 0;
+        float baseVolume = audioSource.volume;
+        while (timer < 1)
+        {
+            audioSource.volume = Mathf.Lerp(baseVolume, 0, timer);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        audioSource.volume = 0;
+    }
+    #endregion
+}
---
+++ Assets/Scripts/MoveObjectsWithDelay.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObjectsWithDelay : MonoBehaviour 
+{
+    #region Public Fields
+    public MovementsAction[] movementsActions = null;
+    public float delay = 0;
+	#endregion
+	
+	#region Private Fields
+	#endregion
+	
+	#region Unity Methods
+	#endregion
+	
+	#region Public Methods
+    public void Execute()
+    {
+        StartCoroutine("Move");
+    }
+    #endregion
+	
+	#region Private Methods
+    private IEnumerator Move()
+    {
+        for(int i = 0; i < movementsActions.Length; i++)
+        {
+            movementsActions[i].ExecuteMovements();
+            yield return new WaitForSeconds(delay);
+        }
+    }
+    #endregion
+}
