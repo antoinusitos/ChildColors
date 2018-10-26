ff3f59c71e0a066b1e626015ba6372336a4f1f56
UBISOFT-ORG\agargasson <antoine.gargasson@ubisoft.com>
2018-10-26 11:07:52 +0200

    added scene3

---
+++ Assets/Scripts/GameStates/ActionComplete.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionComplete : GameState
+{
+    #region Public Fields
+    public bool actionComplete = false;
+    #endregion
+
+    #region Private Fields
+    #endregion
+
+    #region Unity Methods
+    #endregion
+
+    #region Public Methods
+    public override void OnStateEnter()
+    {
+    }
+
+    public override bool OnStateUpdate() //true = exit state
+    {
+        if (actionComplete)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public override void OnStateExit()
+    {
+        base.OnStateExit();
+    }
+
+    public void SetActionComplete(bool newState)
+    {
+        actionComplete = newState;
+    }
+    #endregion
+
+    #region Private Methods
+    #endregion
+}
---
+++ Assets/Scripts/WaitAndExecute.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class WaitAndExecute : MonoBehaviour 
+{
+    #region Public Fields
+    public float timeToWait = 0;
+    public UnityEvent events = null;
+	#endregion
+	
+	#region Private Fields
+	#endregion
+	
+	#region Unity Methods
+	#endregion
+	
+	#region Public Methods
+    public void Execute()
+    {
+        StartCoroutine("Wait");
+    }
+    #endregion
+	
+	#region Private Methods
+    private IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        events.Invoke();
+    }
+    #endregion
+}
