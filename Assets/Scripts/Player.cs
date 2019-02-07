3302636546721ae75efdc473eff853a1fd3e5ffd
UBISOFT-ORG\agargasson <antoine.gargasson@ubisoft.com>
2019-02-07 14:15:05 +0100

    continuing scene 13 and transitions between 12 and 13. Need to do a walkthrough to test

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour 
 {
-	#region Public Fields
+    #region Public Fields
+    public Vector3 startingPos;
 	#endregion
 	
 	#region Private Fields
